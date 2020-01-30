187e832362cdb0b663227ca6ed67621394f797dd
Blothen <looper1520@gmail.com>
2020-01-30 22:36:27 +0000

    updates

--- header.php
+++ header.php
@@ -3,21 +3,31 @@
 ?>
 <!DOCTYPE html>
 <head>
-	<link rel="stylesheet" href="assets/bootstrap.css">
-	<link rel="stylesheet" href="assets/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/bootstrap-grid.css">
-	<link rel="stylesheet" href="assets/custom.css">
-	<link rel="stylesheet" href="assets/smoothproducts.css">
-	<link rel="stylesheet" href="assets/w3.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>TapGoEat</title>
+    <meta name="description" content="Solve Your childs problem, have they forgotten there lunch and all of their money again?">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-grid.css">
+	<link rel="stylesheet" href="assets/css/custom.css">
+	<link rel="stylesheet" href="assets/css/smoothproducts.css">
+	<link rel="stylesheet" href="assets/css/w3.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
 </head>
 <header>
-	<nav id="navbar" class="navbar navbar-light fixed-top bg-white clean-navbar"
-		<div class="container"<a class="logo" href="./">TapGoEat</a>
-		<ul class="nav navbar-nav ml-auto">
-			<li class="nav-item" <a class="nav-link active" href="./">Home</a></li>
-			<li id="navsignin" class="nav-item" <a class="nav-link active" href="./sign-in.php">Sign In</a></li>
-			<li id="navsignup" class="nav-item" <a class="nav-link active" href="./">Sign Up</a></li>
-		</ul>
-		</div>
-	</nav>
+	<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+         <div class="container"><a class="navbar-brand logo" href="./">TapGoEat</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+             <div class="collapse navbar-collapse" id="navcol-1">
+                 <ul class="nav navbar-nav ml-auto">
+                     <li class="nav-item" role="presentation"><a class="nav-link active" href="./">Home</a></li>
+                     <li id="signinnav"class="nav-item" role="presentation"><a class="nav-link active" href="signin.php">Sign In</li>
+                 </ul>
+             </div>
+         </div>
+     </nav>
 </header>
--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 ?>
 
 <body>
-	<main id="homemain" class="w3-padding-large w3-container">
+	<main id="homemain" class="w3-padding-large w3-container w3-center">
+		<br>
+		<br>
+		<br>
+		<br>
 		<p>test</p>
 	</main>
 </body>
