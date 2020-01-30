8c5249bfd80e01ffd83cf819b34bf5ec138e7fa0
Blothen <looper1520@gmail.com>
2020-01-30 22:14:37 +0000

    Setting up page

---
+++ header.php
@@ -0,0 +1,23 @@
+</php	
+	session_start();
+?>
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" href="assets/bootstrap.css">
+	<link rel="stylesheet" href="assets/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/bootstrap-grid.css">
+	<link rel="stylesheet" href="assets/custom.css">
+	<link rel="stylesheet" href="assets/smoothproducts.css">
+	<link rel="stylesheet" href="assets/w3.css">
+</head>
+<header>
+	<nav id="navbar" class="navbar navbar-light fixed-top bg-white clean-navbar"
+		<div class="container"<a class="logo" href="./">TapGoEat</a>
+		<ul class="nav navbar-nav ml-auto">
+			<li class="nav-item" <a class="nav-link active" href="./">Home</a></li>
+			<li id="navsignin" class="nav-item" <a class="nav-link active" href="./sign-in.php">Sign In</a></li>
+			<li id="navsignup" class="nav-item" <a class="nav-link active" href="./">Sign Up</a></li>
+		</ul>
+		</div>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	require 'header.php';
+?>
+
+<body>
+	<main id="homemain" class="w3-padding-large w3-container">
+		<p>test</p>
+	</main>
+</body>
