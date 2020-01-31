9a727bc176f251b86279677e1a9706db9c9f7b3b
Ciaran <looper1520@gmail.com>
2020-01-31 10:34:00 +0000

    Added Footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="page-footer dark">
+        <div class="container">
+            <h5 class="w3-center w3-padding-large">All rights Reserved. This software is given as is, any tampering will lead to legal actions.</h5>
+        </div>
+        <div class="footer-copyright">
+            <p>Copyright &copy; 2018-<?php echo date("Y"); ?> <a href="#">Boyne-Softworks & TheRetroBankers</a>.</p>
+        </div>
+    </footer>
+</body>
+</html>
